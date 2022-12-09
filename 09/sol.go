52426cc29fe0b3c96aa3238028c4c19827bbeeba
nicolst <35232838+nicolst@users.noreply.github.com>
2022-12-09 18:05:52 +0100

    Remove unused code

--- 09/sol.go
+++ 09/sol.go
@@ -7,8 +7,6 @@ import (
 	"strings"
 )
 
-type Direction int
-
 type Pos struct {
 	x int
 	y int
@@ -35,21 +33,6 @@ func Abs(i int) int {
 	return i
 }
 
-func Min(i, j int) int {
-	if i < j {
-		return i
-	}
-	return j
-}
-
-func Sgn(i int) int {
-    if i < 0 {
-        return -1
-    } 
-    return 1
-}
-
-
 func (p1 Pos) GenMove(p2 Pos) Pos {
 	xmove := p2.x - p1.x
 	ymove := p2.y - p1.y
@@ -62,18 +45,14 @@ func (p1 Pos) GenMove(p2 Pos) Pos {
 		return Pos{}
 	}
 
-    if xmove != 0 {
-        xmove /= Abs(xmove)
-    }
-    if ymove != 0 {
-        ymove /= Abs(ymove)
-    }
+	if xmove != 0 {
+		xmove /= Abs(xmove)
+	}
+	if ymove != 0 {
+		ymove /= Abs(ymove)
+	}
 
 	return Pos{xmove, ymove}
-}
-
-type RopeEnd struct {
-	Pos
 }
 
 func check(err error) {
@@ -86,36 +65,36 @@ func Simulate(knots int) int {
 	f, err := os.ReadFile("input.txt")
 	check(err)
 
-    lines := strings.Split(string(f), "\n")
+	lines := strings.Split(string(f), "\n")
 
-    ropeKnots := make([]Pos, knots)
+	ropeKnots := make([]Pos, knots)
 
 	tailPositions := map[int]map[int]bool{}
-    visited := 0
+	visited := 0
 
-    head := &ropeKnots[0]
-    tail := &ropeKnots[knots - 1]
-    for _, line := range lines[:len(lines)-1] {
+	head := &ropeKnots[0]
+	tail := &ropeKnots[knots-1]
+	for _, line := range lines[:len(lines)-1] {
 		move := strings.Split(line, " ")
 		cardinal := Cardinals[move[0]]
 		count, err := strconv.Atoi(move[1])
 		check(err)
 		for i := 0; i < count; i++ {
 			head.Add(cardinal)
-            for i := 1; i < knots; i++{
-                m := ropeKnots[i].GenMove(ropeKnots[i-1])
-                ropeKnots[i].Add(m)
-            }
+			for i := 1; i < knots; i++ {
+				m := ropeKnots[i].GenMove(ropeKnots[i-1])
+				ropeKnots[i].Add(m)
+			}
 			if _, ok := tailPositions[tail.x]; !ok {
 				tailPositions[tail.x] = map[int]bool{}
 			}
-            if _, ok := tailPositions[tail.x][tail.y]; !ok {
-                visited++
-            }
+			if _, ok := tailPositions[tail.x][tail.y]; !ok {
+				visited++
+			}
 			tailPositions[tail.x][tail.y] = true
 		}
 	}
-    return visited
+	return visited
 }
 
 func main() {
